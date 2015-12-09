b52f475078444343bc83092baad27af390434bf4
Martin Gustafsson <gusten86@gmail.com>
2015-12-09 21:33:02 +0100

    Virtual-repeat implementation

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ export function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
     .developmentLogging();
+  aurelia.use.plugin('aurelia-ui-virtualization');
   aurelia.start().then(a => a.setRoot());
 }
